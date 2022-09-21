32df86af6292a4b19ac34f1ba44ee31dca86cc25
Michael Cardell Widerkrantz <mc@mullvad.net>
2022-09-21 11:30:00 +0200

    Make runapp only load and start app

--- cmd/runapp/main.go
+++ cmd/runapp/main.go
@@ -4,13 +4,11 @@
 package main
 
 import (
-	"crypto/ed25519"
 	"fmt"
 	"os"
 
 	"github.com/spf13/pflag"
 	"github.com/tillitis/tillitis-key1-apps/mkdf"
-	"github.com/tillitis/tillitis-key1-apps/mkdfsign"
 	"go.bug.st/serial"
 )
 
@@ -18,8 +16,17 @@ func main() {
 	fileName := pflag.String("file", "", "Name of file to be uploaded")
 	port := pflag.String("port", "/dev/ttyACM0", "Serial port path")
 	speed := pflag.Int("speed", 38400, "When talking over the serial port, bits per second")
+	verbose := pflag.Bool("verbose", false, "")
+
 	pflag.Parse()
-	// mkdf.SilenceLogging()
+	if !*verbose {
+		mkdf.SilenceLogging()
+	}
+
+	if *fileName == "" {
+		fmt.Printf("Usage: Please specify --file path-to-raw-binary\n")
+		os.Exit(1)
+	}
 
 	fmt.Printf("Connecting to device on serial port %s ...\n", *port)
 	conn, err := serial.Open(*port, &serial.Mode{BaudRate: *speed})
@@ -32,50 +39,20 @@ func main() {
 		os.Exit(code)
 	}
 
-	if *fileName != "" {
-		nameVer, err := mkdf.GetNameVersion(conn)
-		if err != nil {
-			fmt.Printf("GetNameVersion failed: %v\n", err)
-			fmt.Printf("If the serial port device is correct, then the device might not be in\n" +
-				"firmware-mode (and already have an app running). Please unplug and plug it in again.\n")
-			exit(1)
-		}
-		fmt.Printf("Firmware has name0:%s name1:%s version:%d\n",
-			nameVer.Name0, nameVer.Name1, nameVer.Version)
-		fmt.Printf("Loading app onto device\n")
-		err = mkdf.LoadAppFromFile(conn, *fileName)
-		if err != nil {
-			fmt.Printf("LoadAppFromFile failed: %v\n", err)
-			exit(1)
-		}
-	} else {
-		fmt.Printf("No app filename given, assuming app is already running\n")
-	}
-
-	pubkey, err := mkdfsign.GetPubkey(conn)
+	nameVer, err := mkdf.GetNameVersion(conn)
 	if err != nil {
-		fmt.Printf("GetPubKey failed: %v\n", err)
+		fmt.Printf("GetNameVersion failed: %v\n", err)
+		fmt.Printf("If the serial port device is correct, then the device might not be in\n" +
+			"firmware-mode (and already have an app running). Please unplug and plug it in again.\n")
 		exit(1)
 	}
-	fmt.Printf("Public Key from device: %x\n", pubkey)
-
-	var message []byte
-	for i := 0; i < 4096; i++ {
-		message = append(message, byte(i))
-	}
-
-	fmt.Printf("Message size: %v, message: %x\n", len(message), message)
-	signature, err := mkdfsign.Sign(conn, message)
+	fmt.Printf("Firmware has name0:%s name1:%s version:%d\n",
+		nameVer.Name0, nameVer.Name1, nameVer.Version)
+	fmt.Printf("Loading app from %v onto device\n", *fileName)
+	err = mkdf.LoadAppFromFile(conn, *fileName)
 	if err != nil {
-		fmt.Printf("Sign failed: %v\n", err)
+		fmt.Printf("LoadAppFromFile failed: %v\n", err)
 		exit(1)
-	}
-	fmt.Printf("Signature over message by device: %x\n", signature)
-
-	if !ed25519.Verify(pubkey, message, signature) {
-		fmt.Printf("Signature did NOT verify.\n")
-	} else {
-		fmt.Printf("Signature verified.\n")
 	}
 
 	exit(0)

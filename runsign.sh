d46496656cf91a21ddc7f57e1e1d428bc2aea069
Daniel Lublin <daniel@lublin.se>
2022-10-18 11:23:07 +0200

    Let runsign.sh take speed optionally

--- runsign.sh
+++ runsign.sh
@@ -1,11 +1,14 @@
 #! /bin/sh
 
-if [ $# -ne 2 ]
-then
-    echo runsign.sh port-device path-to-message
-    exit 1
+if [ $# -ne 2 ] && [ $# -ne 3 ]; then
+  printf "runsign.sh port-device [port-speed] path-to-message\n"
+  exit 2
 fi
 
-./runapp --port $1 --file apps/signerapp/app.bin
-./tk1sign --port $1 --file $2
-
+if [ $# -eq 2 ]; then
+  ./runapp  --port "$1" --file apps/signerapp/app.bin
+  ./tk1sign --port "$1" --file "$2"
+else
+  ./runapp  --port "$1" --speed "$2" --file apps/signerapp/app.bin
+  ./tk1sign --port "$1" --speed "$2" --file "$3"
+fi

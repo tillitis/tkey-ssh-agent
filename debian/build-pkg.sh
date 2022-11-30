c4bf203ec3d1348e6521567bb75a04d64b0aa656
Daniel Lublin <daniel@lublin.se>
2022-11-30 12:53:01 +0100

    Infer debian pkg version from tag in repo
    Signed-off-by: Daniel Lublin <daniel@lublin.se>

--- debian/build-pkg.sh
+++ debian/build-pkg.sh
@@ -7,9 +7,7 @@ set -eu
 # E: tkey-ssh-agent: no-changelog usr/share/doc/tkey-ssh-agent/changelog.Debian.gz (non-native package)
 
 pkgname="tkey-ssh-agent"
-upstream_version="0.1"
 debian_revision="1"
-pkgversion="$upstream_version-$debian_revision"
 pkgmaintainer="Tillitis <hello@tillitis.se>"
 
 if [[ "$(uname -m)" != "x86_64" ]]; then
@@ -23,7 +21,20 @@ rm -rf "$destdir"
 mkdir "$destdir"
 
 pushd ..
+
+upstream_version="$(git describe --dirty --always | sed -n "s/^v\(.*\)/\1/p")"
+if [[ -z "$upstream_version" ]]; then
+  printf "found no tag (with v-prefix) to use for upstream_version\n"
+  exit 1
+fi
+if [[ ! "$upstream_version" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  printf "%s: repo has commit after last tag, or git tree is dirty\n" "$upstream_version"
+  exit 1
+fi
+pkgversion="$upstream_version-$debian_revision"
+
 make tkey-ssh-agent
+
 make DESTDIR="$destdir" \
      PREFIX=/usr \
      SYSTEMDDIR=/usr/lib/systemd \

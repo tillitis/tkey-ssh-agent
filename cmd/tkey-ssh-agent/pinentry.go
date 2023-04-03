711de9e3465acc260b574561f1795271e8514530
Daniel Lublin <daniel@lublin.se>
2023-04-03 12:04:39 +0200

    windows: try to find Gpg4win's pinentry
    Signed-off-by: Daniel Lublin <daniel@lublin.se>

--- cmd/tkey-ssh-agent/pinentry.go
+++ cmd/tkey-ssh-agent/pinentry.go
@@ -5,6 +5,10 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
 
 	"github.com/twpayne/go-pinentry-minimal/pinentry"
 )
@@ -34,6 +38,12 @@ func getSecret(udi string, pinentryProgram string) ([]byte, error) {
 	// If argument is passed, add option to override the pinentry program
 	if pinentryProgram != "" {
 		opts = append(opts, pinentry.WithBinaryName(pinentryProgram))
+	} else if runtime.GOOS == "windows" {
+		found := findWindowsPinentry()
+		if found != "" {
+			le.Printf("Found gpgconf and got pinentry program: %s\n", found)
+			opts = append(opts, pinentry.WithBinaryName(found))
+		}
 	}
 
 	client, err := pinentry.NewClient(opts...)
@@ -48,4 +58,53 @@ func getSecret(udi string, pinentryProgram string) ([]byte, error) {
 		return nil, fmt.Errorf("pinentry GetPin: %w", err)
 	}
 	return []byte(pin), nil
+}
+
+func findWindowsPinentry() string {
+	// When Gpg4win is installed using winget, the path to gpgconf
+	// (and other gpg programs) is added to PATH (it is something like
+	// `C:\Program Files (x86)\GnuPG\bin`). Given that, we try to find
+	// Gpg4win's pinentry program. Inspired by how gpg-agent does it
+	// on Windows, see --pinentry-program on
+	// https://www.gnupg.org/documentation/manuals/gnupg/Agent-Options.html
+
+	var found string
+
+	knownProg, err := exec.LookPath("gpgconf.exe")
+	if err != nil {
+		le.Printf("LookPath: %s\n", err)
+		return ""
+	}
+	// Dropping final "gpgconf.exe"
+	gpgDir := filepath.Dir(knownProg)
+	// Drop final "bin" if present
+	if filepath.Base(gpgDir) == "bin" {
+		gpgDir = filepath.Dir(gpgDir)
+	}
+
+	relExes := []string{`..\Gpg4win\bin\pinentry.exe`, `..\Gpg4win\pinentry.exe`}
+	for _, relExe := range relExes {
+		candidate := filepath.Join(gpgDir, relExe)
+		_, err = os.Stat(candidate)
+		if err != nil {
+			le.Printf("Tried %s got: %s\n", candidate, err)
+			continue
+		}
+		found = candidate
+		break
+	}
+
+	if found == "" {
+		for _, exe := range []string{`pinentry.exe`, `pinentry-basic.exe`} {
+			candidate, err := exec.LookPath(exe)
+			if err != nil {
+				le.Printf("LookPath: %s\n", err)
+				continue
+			}
+			found = candidate
+			break
+		}
+	}
+
+	return found
 }

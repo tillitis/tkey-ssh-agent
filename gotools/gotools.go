46d73f17b850d884ff71ade2a6c276e8bc21b017
Daniel Lublin <daniel@lublin.se>
2023-04-05 13:56:40 +0200

    windows: add manifest/icon to binaries, build the way we want
    Adding windows-agent target to build both windows binaries
    
    Signed-off-by: Daniel Lublin <daniel@lublin.se>

--- gotools/gotools.go
+++ gotools/gotools.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 - Tillitis AB
+// Copyright (C) 2022, 2023 - Tillitis AB
 // SPDX-License-Identifier: GPL-2.0-only
 
 package gotools
@@ -6,4 +6,5 @@ package gotools
 import (
 	// Import tools we use
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/tc-hib/go-winres"
 )

221b97a1001c265eca648f76e2037632af9225ca
Mikael Ågren <mikael@tillitis.se>
2026-03-31 08:54:18 +0200

    test: Make QEmu path configurable from command line
    Add two Pytest command line options to set location of QEmu and
    qemu_usb_mux.py.
    
    The options are output to the test report header so that it is clear
    which locations where used.

--- test/conftest.py
+++ test/conftest.py
@@ -2,12 +2,41 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 from pathlib import Path
-from typing import Iterator
+from typing import Iterator, List
 
 import pytest
 
 from .device import new_qemu_tkey
 from .tt.drivers.tkey import TKey, TKeyType
+
+
+def pytest_addoption(parser: pytest.Parser) -> None:
+    parser.addoption(
+        "--qemu-path", default="qemu-system-riscv32", help="Path to QEmu executable"
+    )
+
+    parser.addoption(
+        "--qemu-usb-mux-path",
+        default="qemu_usb_mux.py",
+        help="Path to TKey USB controller simulator",
+    )
+
+
+def pytest_report_header(config: pytest.Config) -> List[str]:
+    qemu_path = Path(config.getoption("--qemu-path")).expanduser()
+    qemu_usb_mux_path = Path(config.getoption("--qemu-usb-mux-path")).expanduser()
+
+    return [f"qemu-path: {qemu_path}", f"qemu-usb-mux-path: {qemu_usb_mux_path}"]
+
+
+@pytest.fixture(scope="session")
+def qemu_path(request: pytest.FixtureRequest) -> Path:
+    return Path(request.config.getoption("--qemu-path")).expanduser()
+
+
+@pytest.fixture(scope="session")
+def qemu_usb_mux_path(request: pytest.FixtureRequest) -> Path:
+    return Path(request.config.getoption("--qemu-usb-mux-path")).expanduser()
 
 
 @pytest.fixture(

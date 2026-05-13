ae06b7a2682ab31c0a86afd0685ce862c85f0824
Mikael Ågren <mikael@tillitis.se>
2026-05-13 17:13:19 +0200

    test: Test key pairs when getting USS from the default pinentry command on Linux
    Existing USS-via-pinentry tests test using '--uss --pinentry ...'. This
    commit adds Linux-tests for when '--uss' is used on its own. The tests
    are skipped when running on macOS and Windows since pinentry is not the
    default USS-prompt on those platforms.
    
    tkey-ssh-agent will use the 'pinentry' command in PATH. So
    'fake_pinentry.py' is copied to a temporary directory and renamed to
    'pinentry'. The temporary directory is then added to PATH used when
    starting tkey-ssh-agent.

--- test/test_keypair.py
+++ test/test_keypair.py
@@ -1,6 +1,9 @@
 # SPDX-FileCopyrightText: 2026 Tillitis AB <tillitis.se>
 # SPDX-License-Identifier: BSD-2-Clause
 
+import os
+import platform
+import shutil
 from pathlib import Path
 
 import pytest
@@ -20,6 +23,18 @@ USS_PHRASES = [
     "a uss phrase",
     "a very long phrase..." * 10,
 ]
+
+
+@pytest.fixture
+def os_env_with_fake_pinentry_in_path(tmp_path: Path) -> dict[str, str]:
+    env = os.environ.copy()
+    bin_dir = tmp_path / "fake-pinentry"
+    bin_dir.mkdir()
+    shutil.copy2(FAKE_PIN_ENTRY_PATH, bin_dir / "pinentry")
+
+    env["PATH"] = f"{str(bin_dir)}{os.pathsep}{env['PATH']}"
+
+    return env
 
 
 def test_should_show_expected_pubkey_when_not_using_uss(tkey: TKey) -> None:
@@ -45,9 +60,76 @@ def test_should_sign_with_expected_key_when_not_using_uss(
         SshKeygen.verify("a message", signature, expected_pubkey)
 
 
+@pytest.mark.skipif(
+    platform.system() in ["Darwin", "Windows"],
+    reason="test not implemented for macOS or Windows where pinentry is not the default"
+    " USS prompt",
+)
 @pytest.mark.parametrize("uss_phrase", USS_PHRASES)
 @pytest.mark.parametrize("force_full_uss", [FullUss.NO, FullUss.YES])
-def test_should_show_expected_pubkey_when_using_uss(
+def test_should_show_expected_pubkey_when_using_uss_from_default_pinentry(
+    uss_phrase: str,
+    force_full_uss: FullUss,
+    tkey: TKey,
+    tmp_path: Path,
+    os_env_with_fake_pinentry_in_path: dict[str, str],
+) -> None:
+    (tmp_path / "fake-pinentry-pin").write_text(uss_phrase)
+    tkey_ssh_agent = TKeySshAgent(
+        APP_PATH, tkey, cwd=tmp_path, env=os_env_with_fake_pinentry_in_path
+    )
+
+    args = ["--uss"]
+    if force_full_uss == FullUss.YES:
+        args.append("--force-full-uss")
+
+    pubkey = tkey_ssh_agent.show_pubkey(args)
+
+    assert pubkey == derive_expected_pubkey(tkey.type(), uss_phrase, force_full_uss)
+
+
+@pytest.mark.skipif(
+    platform.system() in ["Darwin", "Windows"],
+    reason="test not implemented for macOS or Windows where pinentry is not the default"
+    " USS prompt",
+)
+@pytest.mark.touch
+@pytest.mark.parametrize("uss_phrase", USS_PHRASES)
+@pytest.mark.parametrize("force_full_uss", [FullUss.NO, FullUss.YES])
+def test_should_sign_with_expected_key_when_using_uss_from_default_pinentry(
+    uss_phrase: str,
+    force_full_uss: FullUss,
+    tkey: TKey,
+    tmp_path: Path,
+    os_env_with_fake_pinentry_in_path: dict[str, str],
+) -> None:
+    (tmp_path / "fake-pinentry-pin").write_text(uss_phrase)
+    sock_path = tmp_path / "sock"
+
+    args = ["--agent-path", str(sock_path)]
+    args += ["--uss"]
+    if force_full_uss == FullUss.YES:
+        args.append("--force-full-uss")
+
+    with TKeySshAgent(
+        APP_PATH,
+        tkey,
+        extra_args=args,
+        cwd=tmp_path,
+        env=os_env_with_fake_pinentry_in_path,
+    ):
+        wait_until_path_exists(sock_path, timeout=2)
+
+        expected_pubkey = derive_expected_pubkey(
+            tkey.type(), uss_phrase, force_full_uss
+        )
+        signature = SshKeygen.sign("a message", expected_pubkey, sock_path)
+        SshKeygen.verify("a message", signature, expected_pubkey)
+
+
+@pytest.mark.parametrize("uss_phrase", USS_PHRASES)
+@pytest.mark.parametrize("force_full_uss", [FullUss.NO, FullUss.YES])
+def test_should_show_expected_pubkey_when_using_uss_from_specific_pinentry(
     uss_phrase: str,
     force_full_uss: FullUss,
     tkey: TKey,
@@ -68,7 +150,7 @@ def test_should_show_expected_pubkey_when_using_uss(
 @pytest.mark.touch
 @pytest.mark.parametrize("uss_phrase", USS_PHRASES)
 @pytest.mark.parametrize("force_full_uss", [FullUss.NO, FullUss.YES])
-def test_should_sign_with_expected_key_when_using_uss(
+def test_should_sign_with_expected_key_when_using_uss_from_specific_pinentry(
     uss_phrase: str,
     force_full_uss: FullUss,
     tkey: TKey,
